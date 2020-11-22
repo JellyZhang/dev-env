0bb46caa25262bee4412f7ffc427cbbb50c9f132
Jelly Zhang <zhangjelly0405@gmail.com>
2020-11-22 18:44:13 +0800

    update zshrc

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -52,6 +52,7 @@ rm -rf $home/.oh-my-zsh >> /dev/null
 cp -r ./.oh-my-zsh $home/.oh-my-zsh
 rm $home/.zshrc >> /dev/null
 cp ./.zshrc $home/.zshrc
+echo `export ZSH="${home}/.oh-my-zsh"` >> $home/.zshrc
 rm $home/.p10k.zsh >> /dev/null
 cp ./.p10k.zsh $home/.p10k.zsh
 zsh $home/.zshrc
